5ff238a411b46c88a172d725b02794c7c4eb16c1
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-12-17 17:29:53 -0300

    test: First controller and Razor pages tests

--- SalesWebMVC/SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/SalesWebMVC/Controllers/HomeController.cs
@@ -17,9 +17,10 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC";
+            ViewData["Developer"] = "Johnny Marcelino";
 
-            return View();
+            return View();  // method builder - aux method
         }
 
         public IActionResult Contact()
