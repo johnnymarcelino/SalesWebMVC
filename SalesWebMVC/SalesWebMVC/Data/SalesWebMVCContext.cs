cec565ddb075d635e41fca51ac21abd2d3a6d985
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-12-21 09:52:10 -0300

    feat: Second migration and other entities

--- SalesWebMVC/SalesWebMVC/Data/SalesWebMVCContext.cs
+++ SalesWebMVC/SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -15,6 +15,8 @@ namespace SalesWebMVC.Models
         {
         }
 
-        public DbSet<SalesWebMVC.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Sellers { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
---
+++ SalesWebMVC/SalesWebMVC/Models/Enums/SaleStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesWebMVC.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        Pedding = 0,
+        Billed = 1,
+        Canceled = 2
+    }
+}
