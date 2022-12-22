cca309a139c1ec7fb631ee32e7562484c3aed437
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-12-21 23:49:43 -0300

    feat: SellerService and basic FindAll

---
+++ SalesWebMVC/SalesWebMVC/Services/SellerService.cs
@@ -0,0 +1,21 @@
+﻿using SalesWebMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMVC.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMVCContext _context;  // previne que a dependencia nao seja alterada
+
+        public SellerService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Sellers.ToList();
+        }
+    }
+}
