3ec7d3dc31f4229e5f557a31b3ec8d9e9334933c
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2023-01-17 23:59:21 -0300

    feat: In process the salesrecord class

---
+++ SalesWebMVC/SalesWebMVC/Services/SalesRecordService.cs
@@ -0,0 +1,25 @@
+﻿using SalesWebMVC.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SalesWebMVC.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public SalesRecordService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<SalesRecord> FindByDate(DateTime? minDate, DateTime maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+        }
+    }
+}
